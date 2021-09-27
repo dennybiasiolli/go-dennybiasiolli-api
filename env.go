53cbce9137cb083dd578abd07ddc04f84b351324
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-09-27 14:31:07 +0200

    adding new environment variables for postgres connection

--- env.go
+++ env.go
@@ -9,6 +9,13 @@ import (
 
 var HTTP_LISTEN string
 
+var PG_HOST string
+var PG_PORT string
+var PG_USER string
+var PG_PASSWORD string
+var PG_SSLMODE string
+var PG_DATABASE string
+
 func getEnvVariables() {
 	/*
 		loading .env files in this order, if a variable is not set in `.env`,
@@ -21,4 +28,11 @@ func getEnvVariables() {
 	}
 
 	HTTP_LISTEN = os.Getenv("HTTP_LISTEN")
+
+	PG_HOST = os.Getenv("PG_HOST")
+	PG_PORT = os.Getenv("PG_PORT")
+	PG_USER = os.Getenv("PG_USER")
+	PG_PASSWORD = os.Getenv("PG_PASSWORD")
+	PG_SSLMODE = os.Getenv("PG_SSLMODE")
+	PG_DATABASE = os.Getenv("PG_DATABASE")
 }

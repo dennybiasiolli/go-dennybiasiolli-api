364a80cd0df2a187c66a0413b3bede1214211a8c
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-09-27 16:08:42 +0200

    models: improving Articolo model for gorm

--- models.go
+++ models.go
@@ -1,13 +1,20 @@
 package main
 
-import "time"
+import (
+	"time"
+)
 
 type Articolo struct {
-	ID                int
-	DataPubblicazione time.Time
-	TitoloIt          string
-	TitoloEn          string
-	TestoIt           string
-	TestoEn           string
-	AutoreId          int
+	ID                int       `gorm:"AUTO_INCREMENT; primaryKey"`
+	DataPubblicazione time.Time ``
+	TitoloIt          string    `gorm:"not null; size:255"`
+	TitoloEn          string    `gorm:"size:255"`
+	TestoIt           string    `gorm:"not null;type:text[]"`
+	TestoEn           string    `gorm:"type:text[]"`
+	AutoreId          int       `gorm:"not null"`
+}
+
+// TableName set Articolo's table name to be `news_articolo`
+func (Articolo) TableName() string {
+	return "news_articolo"
 }

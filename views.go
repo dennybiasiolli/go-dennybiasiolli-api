f920e2934cd6b26a0925625bc98e69c4d7de3b6c
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-09-27 20:56:32 +0200

    views: adding specific query and not found error to GetArticol* functions

--- views.go
+++ views.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"errors"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func GetArticoli(c *gin.Context) {
 	var articoli []Articolo
-	db.Find(&articoli)
+	db.Where("data_pubblicazione IS NOT NULL").Find(&articoli)
 	c.JSON(200, articoli)
 }
 func GetArticolo(c *gin.Context) {
 	var articolo Articolo
 	id := c.Params.ByName("id")
-	db.First(&articolo, id)
+	err := db.Where("data_pubblicazione IS NOT NULL").First(&articolo, id).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		c.JSON(http.StatusNotFound, gin.H{"message": err.Error()})
+		return
+	}
 	c.JSON(200, articolo)
 }

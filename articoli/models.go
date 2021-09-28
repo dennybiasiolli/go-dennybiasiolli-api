366641c2d326c8bac27e1c9407e0ed66c386aceb
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-09-28 16:29:06 +0200

    citazioni(models): fixing Citazione and adding AutoMigrate to a new table

--- articoli/models.go
+++ articoli/models.go
@@ -3,13 +3,13 @@ package articoli
 import "time"
 
 type Articolo struct {
-	ID                int       `gorm:"AUTO_INCREMENT; primaryKey"`
-	DataPubblicazione time.Time ``
-	TitoloIt          string    `gorm:"not null; size:255"`
-	TitoloEn          string    `gorm:"size:255"`
-	TestoIt           string    `gorm:"not null;type:text[]"`
-	TestoEn           string    `gorm:"type:text[]"`
-	AutoreId          int       `gorm:"not null"`
+	ID                int        `gorm:"AUTO_INCREMENT; primaryKey"`
+	DataPubblicazione *time.Time ``
+	TitoloIt          string     `gorm:"not null; size:255"`
+	TitoloEn          *string    `gorm:"size:255"`
+	TestoIt           string     `gorm:"not null;type:text"`
+	TestoEn           *string    `gorm:"type:text"`
+	AutoreId          int        `gorm:"not null"`
 }
 
 // TableName set Articolo's table name to be `news_articolo`
--- citazioni/models.go
+++ citazioni/models.go
@@ -1,16 +1,17 @@
 package citazioni
 
 type Citazione struct {
-	ID              int    `gorm:"AUTO_INCREMENT; primaryKey"`
-	Frase           string `gorm:"not null;type:text[]"`
-	Autore          string `gorm:"not null; size:100"`
-	IsPubblica      bool   ``
-	Visualizzazioni uint   ``
-	Likes           uint   ``
-	IsApproved      bool   ``
+	ID              int     `gorm:"AUTO_INCREMENT; primaryKey"`
+	Frase           string  `gorm:"not null;type:text"`
+	Autore          string  `gorm:"not null; size:100"`
+	IsPubblica      bool    `gorm:"not null;"`
+	Visualizzazioni uint    `gorm:"not null;"`
+	Likes           uint    `gorm:"not null;"`
+	IsApproved      bool    `gorm:"not null;"`
+	UserTrackJson   *string `gorm:"type:text"`
 }
 
 // TableName set Citazione's table name to be `citazioni_citazione`
 func (Citazione) TableName() string {
-	return "citazioni_citazione"
+	return "citazioni_citazione_go"
 }

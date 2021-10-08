ed88d0e7ce0e45196f665dc61628fb077cde1302
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-10-08 16:16:20 +0200

    auth(middlewares): adding DjangoJwtAuth

--- auth/middlewares.go
+++ auth/middlewares.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/dennybiasiolli/go-dennybiasiolli-api/common"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 )
 
 func DjangoBasicAuth(c *gin.Context) {
@@ -45,6 +46,47 @@ func DjangoBasicAuth(c *gin.Context) {
 
 	res, err := CheckPassword(authParts[1], user.Password)
 	if !res || err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	c.Set("user", user)
+}
+
+func DjangoJwtAuth(c *gin.Context) {
+	authorizationHeader := c.Request.Header.Get("Authorization")
+	authParts := strings.SplitN(authorizationHeader, " ", 2)
+	if len(authParts) != 2 {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Failed to parse authentication string"})
+		return
+	}
+	if authParts[0] != "Bearer" {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": fmt.Sprintf("Authorization scheme is %v, not Bearer", authParts[0])})
+		return
+	}
+
+	// check token validity
+	token, err := jwt.Parse(authParts[1], func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(common.JWT_HMAC_SAMPLE_SECRET), nil
+	})
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !(ok && token.Valid && claims["token_type"] == "access") {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	db := common.GetDB()
+	var user User = User{
+		IsActive: true,
+	}
+	err = db.Where(&user).First(&user, claims["user_id"]).Error
+	if err != nil {
 		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 		return
 	}

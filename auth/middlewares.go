ade306a4cb15ec19559c312c1405720ac5bee890
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-10-12 10:49:32 +0200

    switching basic authentication check using fiber

--- auth/middlewares.go
+++ auth/middlewares.go
@@ -1,55 +1,23 @@
 package auth
 
 import (
-	"encoding/base64"
-	"errors"
-	"fmt"
-	"net/http"
-	"strings"
-
 	"github.com/dennybiasiolli/go-dennybiasiolli-api/common"
-	"github.com/gin-gonic/gin"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/basicauth"
 	jwtware "github.com/gofiber/jwt/v3"
 	"github.com/golang-jwt/jwt/v4"
 )
 
-func getAuthorizationHeader(c *gin.Context, auth_type string) (string, error) {
-	authorizationHeader := c.Request.Header.Get("Authorization")
-	authParts := strings.SplitN(authorizationHeader, " ", 2)
-	if len(authParts) != 2 {
-		return "", errors.New("Failed to parse authentication string")
-	}
-	if authParts[0] != auth_type {
-		return "", errors.New(fmt.Sprintf("Authorization scheme is %v, not %v", authParts[0], auth_type))
-	}
-	return authParts[1], nil
-}
-
-func DjangoBasicAuth(c *gin.Context) {
-	auth_token, err := getAuthorizationHeader(c, "Basic")
-	if err != nil {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, fiber.Map{"error": err.Error()})
-	}
-
-	sDec, err := base64.StdEncoding.DecodeString(auth_token)
-	if err != nil {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, fiber.Map{"error": "Failed to parse base64 basic credentials"})
-		return
-	}
-	authParts := strings.SplitN(string(sDec), ":", 2)
-	if len(authParts) != 2 {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, fiber.Map{"error": "Failed to parse basic credentials"})
-		return
-	}
-
-	user, err := LoginDjangoUser(authParts[0], authParts[1])
-	if err != nil {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, fiber.Map{"error": "Unauthorized"})
-		return
-	}
-
-	c.Set("user", user)
+func GetDjangoBasicAuthMiddleware() func(*fiber.Ctx) error {
+	return basicauth.New(basicauth.Config{
+		Authorizer: func(username, password string) bool {
+			_, err := LoginDjangoUser(username, password)
+			if err != nil {
+				return false
+			}
+			return true
+		},
+	})
 }
 
 func GetDjangoJwtAuthMiddleware() func(*fiber.Ctx) error {
